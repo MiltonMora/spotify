a9073ab2cfe5469804411a9aa48d5342c17953ba
mayloevan@gmail.com <mayloevan@gmail.com>
2024-07-09 16:56:58 +0200

    primer comit, creacion de clone

---
+++ src/components/Greeting.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  const currenTime = new Date()
+  const currentHour = currenTime.getHours()
+
+  let greeting = "Buenas noches"
+
+  if (currentHour < 12) 
+  {
+    greeting = "Buenos dias"
+  } else if (currentHour < 18) {
+    greeting = "Buenas tardes"
+  }
+
+</script>
+
+<h2 class="text-4xl font-bold">{greeting}</h2>
